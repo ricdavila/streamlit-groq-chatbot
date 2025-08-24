23a430c9a4189c7d5ced98485aaa3dad7ca49208
Ricardo dAvila <ricardo.davila.freitas@gmail.com>
2025-08-24 13:27:09 -0300

    doc: Adicionar melhores comentários e docstrings

--- groq_ia.py
+++ groq_ia.py
@@ -7,14 +7,11 @@ load_dotenv()
 
 client = Groq(api_key=os.getenv('GROQ_API_KEY'))
 
-def gerar_resposta(mensagens):
+def gerar_resposta(mensagens) -> str:
     
     stream = client.chat.completions.create(
         messages=mensagens,
         model="llama-3.3-70b-versatile"
     )
-
-    #resposta = stream.choices[0].message.content
-    #mensagens.append({'role': 'assistant', 'content': resposta})
 
     return stream.choices[0].message.content
--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import time
 import streamlit as st
-
 import groq_ia
 
 APP_TITLE = 'GroqTalk'
@@ -9,24 +8,19 @@ SLEEP_TIME = 0.005
 
 
 def gerar_layout_pagina() -> None:
-    st.session_state.setdefault('mensagens', [{
-        'role': 'system', 'content': 'You are a helpful assistant that speaks portuguese with your users, unless they speak another language.'
-        }])
-
-    st.set_page_config(page_title=APP_TITLE, 
-                    page_icon='💻', 
-                    layout='centered'
-                    )
-
+    '''Carrega os principais widgets e configurações do layout da página'''
+    st.set_page_config(
+        page_title=APP_TITLE, 
+        page_icon='💻', 
+        layout='centered',
+    )    
+    st.markdown(f'<h3><center>{APP_ICON}{APP_TITLE}', unsafe_allow_html=True)
     if len(st.session_state.mensagens) <= 1:
-        st.container(height=100, border=False)
         st.markdown('<h2><center>Olá, posso ajudar?</center>', unsafe_allow_html=True)
-    else:
-        st.markdown(f'<h3><center>{APP_ICON}{APP_TITLE}', unsafe_allow_html=True)
-        st.divider()
 
 
-def escrever_resposta_groq(mensagem):
+def escrever_resposta_groq(mensagem: str) -> None:
+    '''Escreve a resposta do Groq com efeito de digitação'''
     placeholder = st.empty()
     texto = ''
     for char in mensagem:
@@ -36,33 +30,34 @@ def escrever_resposta_groq(mensagem):
 
 
 def adicionar_mensagem(role: str, mensagem: str) -> None:
+    '''Adiciona mensagens ao histórico do chat'''
     st.session_state.mensagens.append({'role': role, 'content': mensagem})
 
 
-def carregar_historico():
-    # carrega e exibe o histórico de mensagens no chat
+def carregar_historico() -> None:
+    '''Carrega e exibe cada mensagem presente no histórico'''
     for mensagem in st.session_state.mensagens:
         if not mensagem['role'] == 'system':
             with st.chat_message(mensagem['role']):
                 st.markdown(mensagem['content'])
 
-def main():
-
+def main() -> None:
+    st.session_state.setdefault('mensagens', [
+        {'role': 'system', 'content': 'You are a helpful assistant that speaks portuguese with your users, unless they speak another language.'}
+    ])
     gerar_layout_pagina()
     carregar_historico()
 
-    # barra de input do usuário
-    chat_input = st.chat_input(placeholder='Peça ao Groq', width='stretch')
+    # input do usuário
+    entrada_usuario = st.chat_input(placeholder='Peça ao Groq', width='stretch')
 
-
-    # adiciona ao chat o input do usuário e a resposta do Groq
-    if chat_input:
-        # adicioan o input ao histórico
-        adicionar_mensagem('user', chat_input)
-        # escreve o input
+    # atualiza o histórico, exibe o input do usuário e escreve a resposta do groq
+    if entrada_usuario:
+        # usuário
+        adicionar_mensagem('user', entrada_usuario)
         with st.chat_message('user'):
-            st.markdown(chat_input)
-        # gera e escreve a resposta do Groq
+            st.markdown(entrada_usuario)
+        # Groq
         with st.chat_message('assistant'):
             with st.spinner('Só um segundo...'):
                 resposta_ia = groq_ia.gerar_resposta(st.session_state.mensagens)
